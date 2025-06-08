afd09e31b48e6b3db87940110247ae1d39d14c6e
ValXux <valenferxux@gmail.com>
2025-06-08 02:30:35 -0500

    prueba de animacion con lottie lab

--- src/screens/Home/HomeScreen.js
+++ src/screens/Home/HomeScreen.js
@@ -1,9 +1,18 @@
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
+import LottieView from 'lottie-react-native';
+
+import styles from './styles';
 
 export default function HomeScreen() {
     return (
         <View>
             <Text>Home Screen</Text>
+            <LottieView
+                source={require('../../../assets/animations/frog_animation.json')}
+                autoPlay
+                loop
+                style={styles.animation}
+            />
         </View>
     );
 }
---
+++ src/screens/Home/styles.js
@@ -0,0 +1,19 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#f0f0f0',
+    },
+    text: {
+        fontSize: 24,
+        color: '#333',
+        marginBottom: 20,
+    },
+    animation: {
+        width: 200,
+        height: 200,
+    },
+});
